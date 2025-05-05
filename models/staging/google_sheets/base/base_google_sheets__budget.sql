01190e82cf6809e43ed07868f0d3af941ac846b4
Isabel Jaldo Ruiz <dbt.jaldoruizisabel@gmail.com>
2025-05-05 17:58:48 +0000

    Added models from google_sheets

--- models/staging/google_sheets/base/base_google_sheets__budget.sql
+++ models/staging/google_sheets/base/base_google_sheets__budget.sql
@@ -1,18 +1,12 @@
-
-{{
-  config(
-    materialized='view'
-  )
-}}
-
 WITH src_budget AS (
     SELECT * 
-    FROM {{ ref('base_google_sheets__budget') }}
+    FROM {{ source('google_sheets', 'budget') }}
     ),
 
 renamed_casted AS (
     SELECT
-          product_id
+        _row
+        , product_id
         , quantity
         , month
         , _fivetran_synced AS date_load
